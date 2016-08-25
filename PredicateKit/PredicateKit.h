f3937bb6fd6bb4919065d4d40df3c31de45953dd
林達也 <t.hayashi0077+github@gmail.com>
2016-08-25 22:23:12 +0900

    Initial Commit

---
+++ PredicateKit/PredicateKit.h
@@ -0,0 +1,19 @@
+//
+//  PredicateKit.h
+//  PredicateKit
+//
+//  Created by 林達也 on 2016/08/25.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PredicateKit.
+FOUNDATION_EXPORT double PredicateKitVersionNumber;
+
+//! Project version string for PredicateKit.
+FOUNDATION_EXPORT const unsigned char PredicateKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PredicateKit/PublicHeader.h>
+
+
